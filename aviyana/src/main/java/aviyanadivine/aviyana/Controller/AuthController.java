1c51b3b2d97794feadd4c5b0a46fb563e0ea59a5
kaushlendra pandey <pandeykaushlendra9628@gmail.com>
2025-10-22 15:53:05 +0530

    new

--- aviyana/src/main/java/aviyanadivine/aviyana/Controller/AuthController.java
+++ aviyana/src/main/java/aviyanadivine/aviyana/Controller/AuthController.java
@@ -5,15 +5,13 @@ import aviyanadivine.aviyana.Entity.PasswordResetRequest;
 import aviyanadivine.aviyana.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins= "*")
 public class AuthController {
 
     @Autowired
